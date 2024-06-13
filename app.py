b8fd6259492b2dd924a355533a36ee9faf7f067b
Simon Sure <info@simonsure.com>
2024-06-13 20:45:37 +0300

    fix runtime dependency
    Signed-off-by: Simon Sure <info@simonsure.com>

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+
 from src.user_routes import user_bp
 
 app = Flask(__name__)
 CORS(app)
+
 app.register_blueprint(user_bp)
 
 @app.route('/')
